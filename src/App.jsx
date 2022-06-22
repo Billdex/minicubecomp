467aaacbe2d8ec8178dcaecfe984d019a25e2f7a
billdex <pc.luoxin@foxmail.com>
2022-06-23 01:55:48 +0800

    vite init, tailwind init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Header from "./components/header";
+
+function App() {
+
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default App
